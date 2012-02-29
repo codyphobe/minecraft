8f457a2dc6bfe93c08ab1f85fa2fc76cc7ab98e7
Nathaniel Blackburn <drummernath@gmail.com>
2012-02-29 22:46:27 +0000

    Optimized the render_skin function and removed comments.

--- class.minecraft.php
+++ class.minecraft.php
@@ -87,25 +87,19 @@ class Minecraft {
                 return imagepng($canvas);
             } else if($render_type == 'body') {
                 header('Content-Type: image/png');
-
-                $scale = $size / 16; //size represents width
+                $scale = $size / 16;
                 $canvas = imagecreatetruecolor(16*$scale, 32*$scale);
                 $image = imagecreatefrompng($this->custom_skin($username));
-
                 imagealphablending($canvas, false);
                 imagesavealpha($canvas,true);
                 $transparent = imagecolorallocatealpha($canvas, 255, 255, 255, 127);
                 imagefilledrectangle($canvas, 0, 0, 16*$scale, 32*$scale, $transparent);
-
-                imagecopyresized  ($canvas, $image, 4*$scale,  0*$scale,  8,   8,   8*$scale,  8*$scale,  8,  8);  //head
-                imagecopyresized  ($canvas, $image, 4*$scale,  8*$scale,  20,  20,  8*$scale,  12*$scale, 8,  12); //body
-                imagecopyresized  ($canvas, $image, 0*$scale,  8*$scale,  44,  20,  4*$scale,  12*$scale, 4,  12); //arm left
-                //imagecopyresized($canvas, $image, 12*$scale, 8*$scale,  44,  20,  4*$scale,  12*$scale, 4,  12); //arm right
-                imagecopyresampled($canvas, $image, 12*$scale, 8*$scale,  47,  20,  4*$scale,  12*$scale, -4,  12); //arm right (flipped)
-                imagecopyresized  ($canvas, $image, 4*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12); //leg left
-                //imagecopyresized($canvas, $image, 8*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12); //leg right
-                imagecopyresampled($canvas, $image, 8*$scale,  20*$scale, 7,   20,  4*$scale,  12*$scale, -4,  12); //leg right (flipped)
-
+                imagecopyresized  ($canvas, $image, 4*$scale,  0*$scale,  8,   8,   8*$scale,  8*$scale,  8,  8);
+                imagecopyresized  ($canvas, $image, 4*$scale,  8*$scale,  20,  20,  8*$scale,  12*$scale, 8,  12);
+                imagecopyresized  ($canvas, $image, 0*$scale,  8*$scale,  44,  20,  4*$scale,  12*$scale, 4,  12);
+                imagecopyresampled($canvas, $image, 12*$scale, 8*$scale,  47,  20,  4*$scale,  12*$scale, -4,  12);
+                imagecopyresized  ($canvas, $image, 4*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12);
+                imagecopyresampled($canvas, $image, 8*$scale,  20*$scale, 7,   20,  4*$scale,  12*$scale, -4,  12);
                 return imagepng($canvas);
             }
         } else {
