325893075aa6cb01d10f209e05f6dcdcaaf99baf
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2012-02-29 13:57:40 +1030

    Small fixes and implemented render_skin function

--- class.minecraft.php
+++ class.minecraft.php
@@ -10,7 +10,7 @@
  * @website: http://nblackburn.me
 */
 
-class minecraft {
+class Minecraft {
 
     public $account;
 
@@ -37,9 +37,9 @@ class minecraft {
     }
 
     public function custom_skin($username) {
-        $headers = get_headers('https://s3.amazonaws.com/MinecraftSkins/'.$username.'.png');
-        if ($headers[7] == 'Content-Type: application/octet-stream') {
-            return 'https://s3.amazonaws.com/MinecraftSkins/'.$username.'.png';
+        $headers = get_headers('http://s3.amazonaws.com/MinecraftSkins/'.$username.'.png');
+        if ($headers[7] == 'Content-Type: image/png' || $headers[7] == 'Content-Type: application/octet-stream') {
+            return 'http://s3.amazonaws.com/MinecraftSkins/'.$username.'.png';
         } else {
             return false;
         }
@@ -67,7 +67,41 @@ class minecraft {
             return false;
         }
     }
+    
+    public function render_skin($username, $render_type, $size) {
+        if ($this->custom_skin($username) != false && in_array($render_type, array('head', 'body'))) {
+            if ($render_type == 'head') {
+                header('Content-Type: image/png');
+                $canvas = imagecreatetruecolor($size, $size);
+                $image = imagecreatefrompng($this->custom_skin($username));
+                imagecopyresampled($canvas, $image, 0, 0, 8, 8, $size, $size, 8, 8);
+                return imagepng($canvas);
+            } else if($render_type == 'body') {
+                header('Content-Type: image/png');
+
+                $scale = $size / 16; //size represents width
+                $canvas = imagecreatetruecolor(16*$scale, 32*$scale);
+                $image = imagecreatefrompng($this->custom_skin($username));
+
+                imagealphablending($canvas, false);
+                imagesavealpha($canvas,true);
+                $transparent = imagecolorallocatealpha($canvas, 255, 255, 255, 127);
+                imagefilledrectangle($canvas, 0, 0, 16*$scale, 32*$scale, $transparent);
+
+                imagecopyresized  ($canvas, $image, 4*$scale,  0*$scale,  8,   8,   8*$scale,  8*$scale,  8,  8);  //head
+                imagecopyresized  ($canvas, $image, 4*$scale,  8*$scale,  20,  20,  8*$scale,  12*$scale, 8,  12); //body
+                imagecopyresized  ($canvas, $image, 0*$scale,  8*$scale,  44,  20,  4*$scale,  12*$scale, 4,  12); //arm left
+                //imagecopyresized($canvas, $image, 12*$scale, 8*$scale,  44,  20,  4*$scale,  12*$scale, 4,  12); //arm right
+                imagecopyresampled($canvas, $image, 12*$scale, 8*$scale,  47,  20,  4*$scale,  12*$scale, -4,  12); //arm right (flipped)
+                imagecopyresized  ($canvas, $image, 4*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12); //leg left
+                //imagecopyresized($canvas, $image, 8*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12); //leg right
+                imagecopyresampled($canvas, $image, 8*$scale,  20*$scale, 7,   20,  4*$scale,  12*$scale, -4,  12); //leg right (flipped)
+
+                return imagepng($canvas);
+            }
+        } else {
+            return false;
+        }
+    }
 
 }
-
-?>
