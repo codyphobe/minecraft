4ae42789bacdd4827b6652571887e3f83ea5abc5
bendem <bendembd@gmail.com>
2013-09-04 18:49:08 +0200

    Update class
    + Renamed $website to $url in `request`
    + Added `CURLOPT_SSL_VERIFYPEER = false` option to curl request
    + Closed curl session before returning (code after return is never
    executed)
    + Added array for easy maintenance of signin errors
    + `is_premium` return now boolean
    + `get_skin` return now the skin url
    + removed duplicate code in `render_skin` and user early return to
    simplify code
    + removed `?>` end tag, [see
    doc](http://php.net/manual/en/language.basic-syntax.phptags.php)

--- class.minecraft.php
+++ class.minecraft.php
@@ -14,41 +14,52 @@ class minecraft {
 
     public $account;
 
-    private function request($website, array $parameters) {
+    private function request($url, array $parameters) {
         $request = curl_init();
         curl_setopt($request, CURLOPT_HEADER, 0);
         curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
         if ($parameters != null) {
-            curl_setopt($request, CURLOPT_URL, $website.'?'.http_build_query($parameters, null, '&'));
+            curl_setopt($request, CURLOPT_URL, $url . '?' . http_build_query($parameters, null, '&'));
         } else {
-            curl_setopt($request, CURLOPT_URL, $website);
+            curl_setopt($request, CURLOPT_URL, $url);
         }
-        return curl_exec($request);
+        $ret = curl_exec($request);
         curl_close($request);
+
+        return $ret;
     }
 
     public function signin($username, $password, $version) {
+    	$error_msg = [
+    		'Account migrated, use e-mail as username.',
+    		'Old version',
+    		'Bad login',
+    		'Bad request'
+    	];
         $parameters = array('user' => $username, 'password' => $password, 'version' => $version);
         $request = $this->request('https://login.minecraft.net/', $parameters);
         $response = explode(':', $request);
-        if ($request != 'Old version' && $request != 'Bad login') {
-            $this->account = array(
-                'current_version' => $response[0],
-                'correct_username' => $response[2],
-                'session_token' => $response[3],
-                'premium_account' => $this->is_premium($username),
-                'player_skin' => $this->get_skin($username),
-                'request_timestamp' => date("dmYhms", mktime(date(h), date(m), date(s), date(m), date(d), date(y)))
-            );
-            return true;
-        } else {
+
+        if (in_array($request, $error_msg)) {
             return false;
         }
+
+        $this->account = array(
+            'current_version' => $response[0],
+            'correct_username' => $response[2],
+            'session_token' => $response[3],
+            'premium_account' => $this->is_premium($response[2]),
+            'player_skin' => $this->get_skin($response[2]),
+            'request_timestamp' => date("dmYhms")
+        );
+
+        return true;
     }
 
     public function is_premium($username) {
         $parameters = array('user' => $username);
-        return $this->request('https://www.minecraft.net/haspaid.jsp', $parameters);
+        return $this->request('https://minecraft.net/haspaid.jsp', $parameters) == 'true';
     }
 
     public function get_skin($username) {
@@ -67,7 +78,7 @@ class minecraft {
     public function keep_alive($username, $session) {
         $parameters = array('name' => $username, 'session' => $session);
         $request = $this->request('https://login.minecraft.net/session', $parameters);
-        return null;
+        return $request;
     }
 
     public function join_server($username, $session, $server) {
@@ -92,14 +103,15 @@ class minecraft {
 
     public function render_skin($username, $render_type, $size) {
         if (in_array($render_type, array('head', 'body'))) {
+            header('Content-Type: image/png');
             if ($render_type == 'head') {
-                header('Content-Type: image/png');
                 $canvas = imagecreatetruecolor($size, $size);
                 $image = imagecreatefrompng($this->get_skin($username));
                 imagecopyresampled($canvas, $image, 0, 0, 8, 8, $size, $size, 8, 8);
+
                 return imagepng($canvas);
-            } else if($render_type == 'body') {
-                header('Content-Type: image/png');
+            }
+            if($render_type == 'body') {
                 $scale = $size / 16;
                 $canvas = imagecreatetruecolor(16*$scale, 32*$scale);
                 $image = imagecreatefrompng($this->get_skin($username));
@@ -113,13 +125,12 @@ class minecraft {
                 imagecopyresampled($canvas, $image, 12*$scale, 8*$scale,  47,  20,  4*$scale,  12*$scale, -4,  12);
                 imagecopyresized  ($canvas, $image, 4*$scale,  20*$scale, 4,   20,  4*$scale,  12*$scale, 4,  12);
                 imagecopyresampled($canvas, $image, 8*$scale,  20*$scale, 7,   20,  4*$scale,  12*$scale, -4,  12);
+
                 return imagepng($canvas);
             }
-        } else {
-            return false;
         }
+
+        return false;
     }
 
 }
-
-?>
